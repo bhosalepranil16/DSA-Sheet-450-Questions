89985500618a60e526fee44bbc0a6fcc1a451033
Dark Lord <bhosalepranil16@gmail.com>
2022-02-13 14:02:51 +0530

    Array Problems 1, 2

---
+++ 01]  Array/01_Reverse_the_Array.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin >> s;
+	
+	    cout << reverseWord(s) << endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  int low = 0, high = str.length() - 1;
+  while(low < high) {
+      swap(str[low], str[high]);
+      low++;
+      high--;
+  }
+  return str;
+}
